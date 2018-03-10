9b53951f537b3199a0823a5212430ae7734cf6a9
Greg Bowler <greg.bowler@g105b.com>
2018-03-10 21:29:01 +0000

    Fix auth bug

--- src/page/_common.php
+++ src/page/_common.php
@@ -4,15 +4,20 @@ namespace App\Page;
 use Gt\WebEngine\Logic\Page;
 
 class _CommonPage extends Page {
+	const NO_AUTH_PATHS = [
+		"/",
+		"/photo",
+		"/data",
+		"/signup",
+	];
+
 	public function go() {
 		$this->checkAuthenticated();
 	}
 
 	protected function checkAuthenticated():void {
 		$path = $this->serverInfo->getRequestUri()->getPath();
-		if($path === "/"
-		|| $path === "/photo"
-		|| $path === "/data") {
+		if(in_array($path, self::NO_AUTH_PATHS)) {
 			return;
 		}
 
